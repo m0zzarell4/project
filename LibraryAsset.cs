800edfe826b5315dc43d0ada0a3bc79cf1ab298c
m0zzarell4 <m0zzarell4oryou@gmail.com>
2025-01-30 14:52:52 +0100

    Create LibraryAsset.cs

---
+++ LibraryAsset.cs
@@ -0,0 +1,9 @@
+namespace project
+{
+    public class LibraryAsset
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
